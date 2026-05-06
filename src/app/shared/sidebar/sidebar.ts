c9c7c464b94f3f195baff0f65dae7b38751ebf3a
Angel <fisiangel.14@gmail.com>
2026-05-06 08:01:53 -0500

    add user to menu sidebar

--- src/app/shared/sidebar/sidebar.ts
+++ src/app/shared/sidebar/sidebar.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../core/services/auth';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  userRole: string | null = null;
+
+  constructor(private authService: AuthService) {
+    this.userRole = this.authService.getRole();
+    }
+}
